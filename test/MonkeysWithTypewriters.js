69b49f506ea35f061f3106e27d5746e2fe6f138b
ashtron <ashton.playsted@protonmail.com>
2022-09-01 13:50:48 -0700

    Adds sentences to stories correctly.

--- test/MonkeysWithTypewriters.js
+++ test/MonkeysWithTypewriters.js
@@ -30,12 +30,27 @@ describe("MonkeysWithTypewriters contract", function() {
         expect(await MonkeysWithTypewriters.ownerOf(1)).to.equal(signer2.address)
     })
 
-    it("Attaches a story when a token is minted", async function() {
+    it("Can add a sentence", async function() {
         const { MonkeysWithTypewriters, signer1, signer2 } = await loadFixture(deployTokenFixture)
 
         await MonkeysWithTypewriters.mint()
-        
         await MonkeysWithTypewriters.addSentence("A strange game.", 0)
-        console.log(await MonkeysWithTypewriters.stories(0, 0))
+        const sentence = await MonkeysWithTypewriters.stories(0, 0)
+
+        expect(sentence[0]).to.equal("A strange game.")
+    })
+
+    it("Stores sentences correctly", async function() {
+        const { MonkeysWithTypewriters, signer1, signer2 } = await loadFixture(deployTokenFixture)
+
+        await MonkeysWithTypewriters.mint()
+
+        await MonkeysWithTypewriters.addSentence("A strange game.", 0)
+        await MonkeysWithTypewriters.addSentence("The only winning move is not to play.", 0)
+
+        const firstSentence = await MonkeysWithTypewriters.stories(0, 0)
+        const secondSentence = await MonkeysWithTypewriters.stories(0, 1)
+
+        expect(`${firstSentence[0]} ${secondSentence[0]}`).to.equal("A strange game. The only winning move is not to play.")
     })
 })
